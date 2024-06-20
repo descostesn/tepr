1ec4881cbbc16888c977d1f5b282e9db297335ed
descoste <nicolas.descostes@embl.it>
2024-06-20 16:59:35 +0200

    add call to Attenuation_fun

--- R/explore.R
+++ R/explore.R
@@ -647,4 +647,5 @@ AUC_KS_Knee_NA.df <- left_join(AUC_allcondi_res, dAUC_allcondi_res,
 AUC_KS_Knee_NA.df <- concat_Diff_mean_res %>% group_by(transcript) %>%
   summarise( chr=chr[1], coor1=min(coor1), coor2=max(coor2), strand=strand[1],
   gene=gene[1], transcript=transcript[1], size=coor2-coor1+1) %>%
-  left_join(AUC_KS_Knee_NA.df, by=c("gene", "transcript", "strand") )
+  left_join(AUC_KS_Knee_NA.df, by=c("gene", "transcript", "strand"))
+tst_df <- Attenuation_fun(AUC_KS_Knee_NA.df, concat_Diff_mean_res, 0.1, "NOT" ) #"NOT" (not replaced) or a number for attenuation (usually 0) or NA # nolint
