d1789a0c0c110203d71a4d3e223f89873324b791
descoste <nicolas.descostes@embl.it>
2024-06-20 17:02:47 +0200

    transform tst_df

--- R/explore.R
+++ R/explore.R
@@ -649,3 +649,13 @@ AUC_KS_Knee_NA.df <- concat_Diff_mean_res %>% group_by(transcript) %>%
   gene=gene[1], transcript=transcript[1], size=coor2-coor1+1) %>%
   left_join(AUC_KS_Knee_NA.df, by=c("gene", "transcript", "strand"))
 tst_df <- Attenuation_fun(AUC_KS_Knee_NA.df, concat_Diff_mean_res, 0.1, "NOT" ) #"NOT" (not replaced) or a number for attenuation (usually 0) or NA # nolint
+
+tst_df <- tst_df %>%
+  mutate(Universe = ifelse(window_size > 50 & Count_NA < 20 &
+    !!sym(mean_value_control_full) > 0.5 & !!sym(mean_value_stress) > 0.5 &
+    !!sym(p_value_theoritical)> 0.1, TRUE, FALSE)) %>%
+  relocate(Universe, .before = 1)
+tst_df <- tst_df %>% mutate(
+    Group = ifelse(Universe == TRUE & !!sym(AUC_stress) > 15 & -log10(!!sym(p_value_KStest)) >1.5, "Attenuated", NA), # nolint
+    Group = ifelse(Universe == TRUE & !!sym(p_value_KStest)>0.2 & !!sym(AUC_ctrl) > -10 & !!sym(AUC_ctrl) < 15 , "Outgroup", Group) # nolint
+  ) %>% relocate(Group, .before = 2)
