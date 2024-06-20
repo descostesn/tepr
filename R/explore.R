e6e0303770744e9bed749a050c44a4687ac0127b
descoste <nicolas.descostes@embl.it>
2024-06-20 16:19:29 +0200

    add AUC_allcondi_fun

--- R/explore.R
+++ R/explore.R
@@ -111,6 +111,7 @@ main_table_read <- function(name_table, extension, working_directory,
 }
 
 
+
 genesECDF <- function(main_table, rounding, expressed_transcript_name_list,
     extension, working_directory) {
 
@@ -209,6 +210,7 @@ genesECDF <- function(main_table, rounding, expressed_transcript_name_list,
 }
 
 
+
 calculates_meanFx <- function(concat_df, window_number) {
 
     res <- getting_var_names(extension, working_directory)
@@ -252,6 +254,8 @@ calculates_meanFx <- function(concat_df, window_number) {
       return(concat_dfFx=concat_df)
 }
 
+
+
 condition_comparison <- function(extension,working_directory) {
 
   res <- getting_var_names(extension, working_directory)
@@ -290,6 +294,8 @@ condition_compared <- function(extension, working_directory,
       }
     }
 }
+
+
 
 Diff_mean_fun <- function(concat_df, dontcompare = NULL) {
 
@@ -408,6 +414,75 @@ dAUC_allcondi_fun <- function(concat_df, window_number, dontcompare) {
   }
   return(dAUC_allcondi)
 }
+
+
+
+# Calculate the Area Under Curve (AUC), All conditions vs y=x 
+# Calculate Mean Value over the full gene body in All conditions.
+
+AUC_allcondi_fun <- function(concat_df,window_number) {
+
+  # Load a library using require()
+  if (!require(pracma)) {
+    install.packages("pracma")
+    library(pracma)
+  }
+
+
+  AUC_allcondi <- concat_df %>% filter(window==round(window_number/2))  %>% mutate(window_size = abs(coor2-coor1), .keep = "all") %>% select("transcript", "gene", "strand", "window_size") %>% distinct() # nolint
+  res <- getting_var_names(extension, working_directory)
+  Conditions <- res$Conditions
+  n <- window_number
+  cumulative_density <- seq(1, n) / n
+
+  for (cond in Conditions) {
+    #already present columns
+    diff_Fx_condi_name <- paste0("diff_Fx_", cond)
+    mean_value_condi_name <- paste0("mean_value_", cond)
+    mean_Fx_condi_name <- paste0("mean_Fx_", cond)
+
+    df_name <- paste0("gene_summary_AUC_", cond)
+    assign(df_name,data.frame())
+
+    # Get the data frame using the dynamically generated name
+    AUC_summary_condi_df <- data.frame()
+    AUC_summary_condi_df <- get(df_name)
+
+    # new column
+    AUC <- paste0("AUC_", cond)
+    p_AUC <- paste0("p_AUC_", cond)
+    D_AUC <- paste0("D_AUC_", cond)
+    MeanValueFull_condi_name <- paste0("MeanValueFull_", cond) #mean value over the full gene body
+
+    AUC_summary_condi_df <- concat_df %>% group_by(transcript) %>%
+      arrange(coord) %>%
+      reframe(gene=gene[1], 
+              !!AUC := trapz(coord,!!sym(diff_Fx_condi_name)) ,
+              !!p_AUC := ks.test(!!sym(mean_Fx_condi_name),cumulative_density)$p.value, # nolint
+              !!D_AUC := ks.test(!!sym(mean_Fx_condi_name),cumulative_density)$statistic, # nolint
+              strand = strand, transcript=transcript,
+              !!(MeanValueFull_condi_name) :=mean(!!sym(mean_value_condi_name))) %>% # nolint
+      dplyr::distinct()
+    assign(df_name, AUC_summary_condi_df)
+    AUC_allcondi <- left_join(AUC_allcondi, AUC_summary_condi_df, by = c("transcript", "gene","strand")) # nolint
+  }
+
+  AUC_allcondi <- AUC_allcondi %>%
+  mutate(across(contains("p_AUC"), ~ modify_p_values(.)))
+
+  # Get the column names containing "p_dAUC"
+  p_AUC_columns <- grep("p_AUC", colnames(AUC_allcondi), value = TRUE)
+
+  # Loop through each column, calculate adjusted p-values, and create new columns
+  for (col_name in p_AUC_columns) {
+    print(col_name)
+    adjusted_col_name <- paste0("adjFDR_", col_name)
+    AUC_allcondi[[adjusted_col_name]] <- p.adjust(AUC_allcondi[[col_name]], method = "fdr") # nolint
+  }
+  return(AUC_allcondi)
+}
+# rm(AUC_allcondi)
+
 
 
 ##################
