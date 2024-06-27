ffc2e7bb1e62663d581a8012a5595e62c920d827
Nicolas Descostes <nicolas.descostes@embl.it>
2024-06-27 12:54:14 +0200

    format

--- BashAndR/explore.sh
+++ BashAndR/explore.sh
@@ -4,7 +4,11 @@ cd /g/romebioinfo/Projects/tepr/downloads/annotations
 
 ## Create bed of protein coding genes from gencode file
 grep -w transcript gencode.v43.basic.annotation.gtf | grep -w MANE_Select > MANE_Select.protein_coding.gtf
-awk 'OFS="\t" {print $0}' MANE_Select.protein_coding.gtf | tr -d '";' | sort -k1,1 -k2,2n  | awk -F \t -v OFS='\t' '{print $0}' | awk -v OFS="\t" '{ print $1,$4,$5,$12,$16,$7}' > MANE_Select.protein_coding.bed
+
+awk 'OFS="\t" {print $0}' MANE_Select.protein_coding.gtf | tr -d '";' | \
+sort -k1,1 -k2,2n  | awk -F \t -v OFS='\t' '{print $0}' | \
+awk -v OFS="\t" '{ print $1,$4,$5,$12,$16,$7}' > MANE_Select.protein_coding.bed
+
 rm MANE_Select.protein_coding.gtf
 
 ## Create bed of lncRNA genes from gencode file
@@ -16,7 +20,16 @@ grep -v 'transcript_support_level "4"' > Ensembl_canonical_TSL123.lncRNA.gtf
 awk 'OFS="\t" {print $0}' Ensembl_canonical_TSL123.lncRNA.gtf | tr -d '";' | \
 sort -k1,1 -k2,2n  | awk -F \t -v OFS='\t' '{print $0}' | \
 awk -v OFS="\t" '{ print $1,$4,$5,$12,$16,$7}' > Ensembl_canonical_TSL123.lncRNA.bed
+
 rm Ensembl_canonical_TSL123.lncRNA.gtf
 
 ## Removing blacklist from bed files. The black list was downloaded from
 ## https://github.com/Boyle-Lab/Blacklist/blob/master/lists/hg38-blacklist.v2.bed.gz
+WORKING="/Users/victor/Documents/DATA/annotation/V43"
+blacklist="/Users/victor/Documents/DATA/Repeatmasker/blacklist/hg38-blacklist.v2.sorted.bed"
+window=200
+
+mkdir makewindow
+awk -F "\t" -v OFS="\t" '{print $1,$2,$3,$4"_"$5"_"$6}' MANE_Select.protein_coding.bed | bedtools intersect -a stdin -b $blacklist -v  | bedtools makewindows -n $window -i srcwinnum -b stdin | sort -k1,1 -k2,2n > $WORKING/makewindow/v43.MANE_protein.window${window}.bed  
+
+echo $WORKING/makewindow/v43.MANE_protein.window${window}.bed 
