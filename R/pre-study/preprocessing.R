31327c738fe0d6c3be5397a395a3195d280bd8fa
descoste <nicolas.descostes@embl.it>
2024-07-17 16:50:05 +0200

    add saverds

--- R/pre-study/preprocessing.R
+++ R/pre-study/preprocessing.R
@@ -242,6 +242,12 @@ lncrnawindows <- makewindowsbedtools(lncrnanoblacknomapgr, windsize)
 exptab <- read.csv(exptabpath, header = TRUE)
 protcoddf <- buildscoreforintervals(protcodwindows, exptab, "protein_coding",
     nbcpu, database_name)
+saveRDS(protcoddf, file = file.path(robjoutputfold,
+    "protcoddffrompreprocessing.rds"))
 lncrnadf <- buildscoreforintervals(lncrnawindows, exptab, "lncrna", nbcpu,
     database_name)
+saveRDS(lncrnadf, file = file.path(robjoutputfold,
+    "lncrnadffrompreprocessing.rds"))
 alldf <- rbind(protcoddf, lncrnadf)
+saveRDS(alldf, file = file.path(robjoutputfold,
+    "alldffrompreprocessing.rds"))
