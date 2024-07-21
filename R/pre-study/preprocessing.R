c2f4767af8f353e70d73db7550db2ff39e2f8e30
descoste <nicolas.descostes@embl.it>
2024-07-21 17:55:56 +0200

    create checkexptab

--- R/pre-study/preprocessing.R
+++ R/pre-study/preprocessing.R
@@ -51,6 +51,27 @@ createfolder <- function(outfold) {
         dir.create(outfold, recursive = TRUE)
 }
 
+checkexptab <- function(exptab) {
+    colnamevec <- c("condition", "direction", "path", "replicate", "strand")
+    if (!isTRUE(all.equal(sort(colnames(exptab)), colnamevec)))
+        stop("The experiment table should have the columns: ",
+            "'condition', 'direction', 'path', 'replicate', 'strand'")
+
+    if (!isTRUE(all.equal(length(unique(exptab$condition)), 2)))
+        stop("The table should only contain two conditions")
+
+    directionvec <- unique(exptab$direction)
+    if (!isTRUE(all.equal(length(directionvec), 2)) ||
+        !isTRUE(all.equal(directionvec, c("fwd", "rev"))))
+        stop("Only two values are allowed for the column direction of the",
+            "experiment table, 'fwd' and 'rev'")
+
+    strandvec <- unique(exptab$strand)
+    if (!isTRUE(all.equal(strandvec, c("+", "-"))))
+        stop("The strand column of the experiment table should only contain",
+            " '+' and '-'.")
+}
+
 createblacklist <- function(blacklistname, outputfolder) { # nolint
 
     blacklistgr <- AnnotationHub::query(AnnotationHub::AnnotationHub(),
@@ -97,30 +118,8 @@ createfolder(robjoutputfold)
 
 ## Reading the information about experiments
 exptab <- read.csv(exptabpath, header = TRUE)
+checkexptab(exptab)
 
-!!!!!!!!!!!
-
-colnamevec <- c("condition", "direction", "path", "replicate", "strand")
-if (!isTRUE(all.equal(sort(colnames(exptab)), colnamevec)))
-    stop("The experiment table should have the columns: ",
-        "'condition', 'direction', 'path', 'replicate', 'strand'")
-
-if (!isTRUE(all.equal(length(unique(exptab$condition)), 2)))
-    stop("The table should only contain two conditions")
-
-directionvec <- unique(exptab$direction)
-if (!isTRUE(all.equal(length(directionvec), 2)) ||
-    !isTRUE(all.equal(directionvec, c("fwd", "rev"))))
-    stop("Only two values are allowed for the column direction of the",
-        "experiment table, 'fwd' and 'rev'")
-
-strandvec <- unique(exptab$strand))
-if (!isTRUE(all.equal(strandvec, c("+", "-"))))
-    stop("The strand column of the experiment table should only contain",
-        " '+' and '-'.")
-
-
-!!!!!!!!!!!!
 ## Read gencode file
 gencode <- read.delim(gencodepath, header = FALSE, skip = 5)
 gencode <- gencode[which(gencode$V3 == "transcript"), ]
