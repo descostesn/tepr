e1a6d8f82764e12d1b5bf6fa3d7536f9a4011d50
descoste <nicolas.descostes@embl.it>
2024-07-21 15:33:17 +0200

    move up function and adapt folder

--- R/pre-study/documentation/explore.R
+++ R/pre-study/documentation/explore.R
@@ -18,44 +18,6 @@ rounding <- 10
 #FUNCTIONS
 ##################
 
-
-getting_var_names <- function(extension, working_directory) {
-
-  bedgraph_files <- list.files(working_directory, pattern = extension,
-    full.names = TRUE)
-  files <- bedgraph_files %>% map(~{filename <- tools::file_path_sans_ext(basename(.))}) # nolint
-  string <- files
-  var_names <- string
-
-  for (i in seq_along(var_names)) {
-    if (grepl("(reverse|forward)", var_names[i])) {
-      var_names[i] <- gsub("reverse", "minus", var_names[i])
-      var_names[i] <- gsub("forward", "plus", var_names[i])
-    }
-  }
-
-  # Extract conditions
-  Conditions <- unique(sub("(\\w+)_rep\\d+.*", "\\1", var_names)) ## verify it can work with several "_" # nolint
-
-  # Extract replication numbers
-  replicate_numbers <- unique(sub(".*_rep(\\d+).*", "\\1", var_names))
-
-  # Setting fixed column names
-  fixed_names <- c("biotype","chr", "coor1", "coor2","transcript", "gene", "strand","window","id") #biotype is added in the .tsv file # nolint
-
-  num_fixed_cols <- length(fixed_names)
-  # Number of variable columns based on input file
-  num_var_cols <- ncol(table) - num_fixed_cols
-  #Generate variable column names for category 2, alternating with category 1
-  test <- rep(var_names, each=2)
-  suffix <- rep(c("", "_score"), length(var_names))
-  test <- paste0(test, suffix)
-  col_names <- c(fixed_names, test)
-
-  return(list(col_names=col_names,var_names=var_names, replicate_numbers=replicate_numbers, Conditions=Conditions)) # nolint
-}
-
-# expression_threshold=0.1
 main_table_read <- function(name_table, extension, working_directory,
     expression_threshold) {
 
@@ -109,6 +71,45 @@ main_table_read <- function(name_table, extension, working_directory,
 
     return(list(main_table=main_table,expressed_transcript_name_list=expressed_transcript_name_list)) # nolint
 }
+
+
+getting_var_names <- function(extension, working_directory) {
+
+  bedgraph_files <- list.files(working_directory, pattern = extension,
+    full.names = TRUE)
+  files <- bedgraph_files %>% map(~{filename <- tools::file_path_sans_ext(basename(.))}) # nolint
+  string <- files
+  var_names <- string
+
+  for (i in seq_along(var_names)) {
+    if (grepl("(reverse|forward)", var_names[i])) {
+      var_names[i] <- gsub("reverse", "minus", var_names[i])
+      var_names[i] <- gsub("forward", "plus", var_names[i])
+    }
+  }
+
+  # Extract conditions
+  Conditions <- unique(sub("(\\w+)_rep\\d+.*", "\\1", var_names)) ## verify it can work with several "_" # nolint
+
+  # Extract replication numbers
+  replicate_numbers <- unique(sub(".*_rep(\\d+).*", "\\1", var_names))
+
+  # Setting fixed column names
+  fixed_names <- c("biotype","chr", "coor1", "coor2","transcript", "gene", "strand","window","id") #biotype is added in the .tsv file # nolint
+
+  num_fixed_cols <- length(fixed_names)
+  # Number of variable columns based on input file
+  num_var_cols <- ncol(table) - num_fixed_cols
+  #Generate variable column names for category 2, alternating with category 1
+  test <- rep(var_names, each=2)
+  suffix <- rep(c("", "_score"), length(var_names))
+  test <- paste0(test, suffix)
+  col_names <- c(fixed_names, test)
+
+  return(list(col_names=col_names,var_names=var_names, replicate_numbers=replicate_numbers, Conditions=Conditions)) # nolint
+}
+
+
 
 
 
@@ -623,7 +624,8 @@ Attenuation_fun <- function(AUC_KS_Knee_NA_DF, concat_df, pval,Replaced) {
 # MAIN
 ##################
 
-results_main_table <- main_table_read(name_table, extension, working_directory, 0.1) # nolint
+results_main_table <- main_table_read(name_table, extension,
+  file.path(working_directory, bedgraph), 0.1) # nolint
 resultsECDF <- genesECDF(main_table = results_main_table[[1]], rounding,
     expressed_transcript_name_list = results_main_table[[2]], extension,
     working_directory)
