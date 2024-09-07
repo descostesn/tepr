5992cd048cd5131f0f6e341f18d427e57691d70b
descoste <nicolas.descostes@embl.it>
2024-09-07 14:52:16 +0200

    code until window size factor

--- R/pre-study/figure2d.R
+++ R/pre-study/figure2d.R
@@ -11,7 +11,7 @@
 ##################
 
 
-#completedfpath <- "/g/romebioinfo/tmp/downstream/completedf.rds"
+completedfpath <- "/g/romebioinfo/tmp/downstream/completedf.rds"
 dfmeandiffpath <- "/g/romebioinfo/tmp/downstream/dfmeandiff.rds"
 exptabpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/exptab.csv" # nolint
 colvec <- c("#90AFBB", "#FF9A04", "#10AFBB", "#FC4E07")
@@ -23,15 +23,20 @@ genename <- "EGFR"
 ##################
 
 ## Reading inputs
-#completedf <- readRDS(completedfpath)
+completedf <- readRDS(completedfpath)
 dfmeandiff <- readRDS(dfmeandiffpath)
 expdf <- read.csv(exptabpath, header = TRUE)
 
-plotecdf <- function(dfmeandiff, genename, verbose = TRUE) {
+plotecdf <- function(dfmeandiff, completedf, genename, verbose = TRUE) {
 
     ## Retrieving rows concerning the gene of interest
     if (verbose) message("\t Retrieving rows concerning the gene of interest")
     df <- dfmeandiff[which(dfmeandiff$gene == genename), ]
 
-    ## R
+    ## Retrieving gene info
+    geneinfo <- completedf[which(completedf$gene == genename), ]
+
+    ## Computing the window size factor
+    df100 <- df[which(df$coord == 100), ]
+    windsizefact <- (df100$end - df100$start) / 1000
 }
