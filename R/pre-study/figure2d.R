cfa8b88ae2f7a643653529a1734bcaab93b0676f
descoste <nicolas.descostes@embl.it>
2024-09-07 15:08:08 +0200

    need allauc and completedf recompute

--- R/pre-study/figure2d.R
+++ R/pre-study/figure2d.R
@@ -5,6 +5,9 @@
 ## Descostes - R-4.4.1 - sept 2024
 ########################
 
+library("ggplot2")
+options(digits = 2)
+
 
 ##################
 # PARAMETERS
@@ -39,4 +42,32 @@ plotecdf <- function(dfmeandiff, completedf, genename, verbose = TRUE) {
     ## Computing the window size factor
     df100 <- df[which(df$coord == 100), ]
     windsizefact <- (df100$end - df100$start) / 1000
+
+
+    !!!!!!!!!!
+
+    AUC_cond_name <- paste0("AUC_", cond)
+    AUC_cond <- round(gene_stat %>% pull(AUC_cond_name), decimal_places)  # Round AUC value
+    KS_cond_name <-  paste0("adjFDR_p_AUC_", cond)
+    KS_cond <- formatC(gene_stat %>% pull(KS_cond_name), format = "e", digits = decimal_places)
+    knee_cond_name <- paste0("knee_AUC_", cond)
+    knee_cond <- round(gene_stat %>% mutate(knee_kb=!!sym(knee_cond_name)*window_size/1000) %>% pull(knee_kb))  # Round Knee value
+    
+
+    # If KS test value is less than 0.01, store the knee_cond value for the vertical line
+    
+    if (as.numeric(KS_cond) < 0.01) {
+      vline_data <- rbind(vline_data, data.frame(Condition = cond, Knee_Value = knee_cond))
+      
+    } else {
+      knee_cond <- NA
+    }
+    
+    sapply(unique(expdf$condition), function(cond) {
+        return(paste0(geneinfo[,paste0("auc_", cond)], geneinfo[,paste0("adjFDR_", cond)]))
+    })
+    conditiontext <- paste0(cond, ": AUC = ", AUC_cond, ", KS = ", KS_cond, ", Knee (kb) = ", knee_cond)
+    subtitle_text <- paste(subtitle_text, condition_text, sep = "\n")
+
+    !!!!!!!!!!!!
 }
