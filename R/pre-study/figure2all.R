9f457f6b86ced2a3310297cc8bd3dc61309a8a10
descoste <nicolas.descostes@embl.it>
2024-09-08 14:31:57 +0200

    initial commit

---
+++ R/pre-study/figure2all.R
@@ -0,0 +1,6 @@
+########################
+## This script aims at reproducing the panels of figure 2 (excluding panel d)
+## of the article using the objects XX generated with the script downstream.R
+##
+## Descostes - R-4.4.1 - sept 2024
+########################
