3b40bdaea4d0a65c57a3cac2baa1f74eece5cd73
descoste <nicolas.descostes@embl.it>
2024-09-13 11:20:07 +0200

    tmp

--- R/pre-study/conformity/conformitynewtest.R
+++ R/pre-study/conformity/conformitynewtest.R
@@ -4,6 +4,7 @@ library("rtracklayer")
 bgvicpath <- "/g/romebioinfo/Projects/tepr/testfromscratch/bedgraph255/protein_coding_score/ctrl_rep1.forward.window200.MANE.wmean.name.score"
 
 allbgnicpath <- "/g/romebioinfo/tmp/preprocessing/backup/bedgraphwmeanlist.rds"
+allwindowspath <- "/g/romebioinfo/tmp/preprocessing/allwindowsbed.rds"
 
 bgvic <- read.delim(bgvicpath, header = FALSE)
 allbgnic <- readRDS(allbgnicpath)
