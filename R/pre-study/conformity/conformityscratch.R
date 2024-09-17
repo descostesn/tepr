629bf6452ff472a7ec660eae63bf81693be8f9f4
descoste <nicolas.descostes@embl.it>
2024-09-17 10:26:00 +0200

    read blacklist and maptrack

--- R/pre-study/conformity/conformityscratch.R
+++ R/pre-study/conformity/conformityscratch.R
@@ -1,10 +1,47 @@
 library("rtracklayer")
+library("GenomicRanges")
 
+##################
+# PARAMETERS
+##################
 
 bgvicpath <- "/g/romebioinfo/Projects/tepr/testfromscratch/bedgraph255/protein_coding_score/ctrl_rep1.forward.window200.MANE.wmean.name.score"
 
 allbgnicpath <- "/g/romebioinfo/tmp/preprocessing/backup/bedgraphwmeanlist.rds"
 allwindowspath <- "/g/romebioinfo/tmp/preprocessing/allwindowsbed.rds"
+
+blacklistshpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/hg38-blacklist.v2.bed" # nolint
+maptrackpath <- "/g/romebioinfo/tmp/preprocessing/maptrackgr.gr"
+
+##################
+#FUNCTIONS
+##################
+
+bedtogr <- function(currentbed, strand = TRUE, symbol = TRUE,
+    allwindows = FALSE) {
+
+    if (!allwindows) {
+        grres <- GenomicRanges::GRanges(seqnames = currentbed[, 1],
+                ranges = IRanges::IRanges(start = currentbed[, 2],
+                                      end = currentbed[, 3],
+                                      names = currentbed[, 4]),
+                strand = if (strand) currentbed[, 6] else "*",
+                symbol = if (symbol) currentbed[, 5] else NA)
+    } else {
+        grres <- GenomicRanges::GRanges(seqnames = currentbed[, 2],
+                ranges = IRanges::IRanges(start = currentbed[, 3],
+                                      end = currentbed[, 4],
+                                      name = currentbed[, 5]),
+                gene = currentbed[, 6], strand = currentbed[, 7],
+                biotype = currentbed[, 1], window = currentbed[, 8],
+                coord = currentbed[, 9])
+    }
+    return(grres)
+}
+
+##################
+# MAIN
+##################
 
 ## This is the ctrl rep1 fwd
 bgvic <- read.delim(bgvicpath, header = FALSE)
@@ -21,3 +58,9 @@ allwindowsbed <- readRDS(allwindowspath)
 bgvicarf <- bgvic[which(bgvic$V6 == "ARF5"), ]
 bgnicarf <- bgvic[which(bgnic$gene == "ARF5"), ]
 allwindarf <- allwindowsbed[which(allwindowsbed$gene == "ARF5"), ]
+allwindowsgr <- bedtogr(allwindarf, allwindows = TRUE)
+
+## Reading black list and maptrack
+blacklistbed <- read.delim(blacklistshpath, header = FALSE)
+blacklistgr <- bedtogr(blacklistbed, strand = FALSE, symbol = FALSE)
+maptrackgr <- readRDS(maptrackpath)
