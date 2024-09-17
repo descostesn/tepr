496e26473f414fbeb384904656f85c620664555d
descoste <nicolas.descostes@embl.it>
2024-09-17 10:15:33 +0200

    retrieve data before debugging

--- R/pre-study/conformity/conformityscratch.R
+++ R/pre-study/conformity/conformityscratch.R
@@ -6,5 +6,18 @@ bgvicpath <- "/g/romebioinfo/Projects/tepr/testfromscratch/bedgraph255/protein_c
 allbgnicpath <- "/g/romebioinfo/tmp/preprocessing/backup/bedgraphwmeanlist.rds"
 allwindowspath <- "/g/romebioinfo/tmp/preprocessing/allwindowsbed.rds"
 
+## This is the ctrl rep1 fwd
 bgvic <- read.delim(bgvicpath, header = FALSE)
+
+## Selecting ctrl rep1 fwd
 allbgnic <- readRDS(allbgnicpath)
+names(allbgnic) <- gsub(".bg","",basename(names(allbgnic)))
+bgnic <- allbgnic[["ctrl_rep1.forward"]]
+
+## Reading all windows bed
+allwindowsbed <- readRDS(allwindowspath)
+
+## Selecting the lines corresponding to the gene ARF5
+bgvicarf <- bgvic[which(bgvic$V6 == "ARF5"), ]
+bgnicarf <- bgvic[which(bgnic$gene == "ARF5"), ]
+allwindarf <- allwindowsbed[which(allwindowsbed$gene == "ARF5"), ]
