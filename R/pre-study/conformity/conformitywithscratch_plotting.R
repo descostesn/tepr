95cba446dc440ad8605f43e2cb4edc5a7e1af88a
descoste <nicolas.descostes@embl.it>
2024-09-26 18:35:11 +0200

    change colvec

--- R/pre-study/conformity/conformitywithscratch_plotting.R
+++ R/pre-study/conformity/conformitywithscratch_plotting.R
@@ -12,7 +12,7 @@ library("tidyselect")
 unigroupdfpath <- "/g/romebioinfo/tmp/comparewithscratch-downstream/niccode_unigroupdf.rds" # nolint
 dfmeandiffpath <- "/g/romebioinfo/tmp/comparewithscratch-downstream/niccode_dfmeandiffvic.rds" # nolint
 expdfpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/exptab-bedgraph-vicnames.csv" # nolint
-colvec <- c("#90AFBB", "#FF9A04", "#10AFBB", "#FC4E07")
+colvec <- c("#90AFBB", "#10AFBB", "#FF9A04", "#FC4E07")
 outfold <- "/g/romebioinfo/tmp/comparewithscratch-plotting"
 
 
