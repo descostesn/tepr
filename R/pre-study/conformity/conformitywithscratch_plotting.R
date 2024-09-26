d924ce82841884d17b95acd0a06e79ab80d1ea7a
descoste <nicolas.descostes@embl.it>
2024-09-26 19:14:01 +0200

    rename .callggplot to .callggplotecdf

--- R/pre-study/conformity/conformitywithscratch_plotting.R
+++ R/pre-study/conformity/conformitywithscratch_plotting.R
@@ -55,7 +55,7 @@ outfold <- "/g/romebioinfo/tmp/comparewithscratch-plotting"
             paste("value", cond, paste0("rep", rep), "score", sep = "_") })})))
 }
 
-.callggplot <- function(dflongecdf, colvec, windsizefact, vlinedf, subtext,
+.callggplotecdf <- function(dflongecdf, colvec, windsizefact, vlinedf, subtext,
     outfold, genename, kneeval, plot) {
 
     colvec <- as.vector(factor(dflongecdf$conditions, labels = colvec))
@@ -152,7 +152,7 @@ plotecdf <- function(dfmeandiff, unigroupdf, expdf, genename, colvec, outfold, #
 
     ## Plotting
     if (verbose && !plot) message("\t Generating ecdf plot to ", outfold)
-    .callggplot(dflongecdf, colvec, windsizefact, vlinedf, subtext, outfold,
+    .callggplotecdf(dflongecdf, colvec, windsizefact, vlinedf, subtext, outfold,
         genename, kneeval, plot)
 }
 
@@ -174,3 +174,81 @@ expdf <- read.csv(expdfpath, header = TRUE)
 
 plotecdf(dfmeandiff, unigroupdf, expdf, "EGFR", colvec, outfold, plot = TRUE)
 plotecdf(dfmeandiff, unigroupdf, expdf, "MARCHF6", colvec, outfold, plot = TRUE)
+
+
+####
+#### plotauc
+####
+
+!!!!!!!!!!!!!
+
+plotauc <- function(tab, auc_ctrlname = "AUC_ctrl", auc_stressname = "AUC_HS", # nolint
+    pvalkstestcolname = "adjFDR_p_dAUC_Diff_meanFx_HS_ctrl",
+    labelx = "AUC in Control", labely = "AUC in Stress", axismin_x = -10,
+    axismax_x = 100, axismin_y = -10, axismax_y = 100, maintitle = "",
+    subtitle = "", legendpos = "bottom", formatname = "pdf", outfold = "./",
+    outfile = "AUCcompare_pval.pdf", plottype = "pval", plot = FALSE,
+    universename = "universe", groupname = "group") {
+
+        if (!isTRUE(all.equal(plottype, "pval")) &&
+            !isTRUE(all.equal(plottype, "groups")))
+            stop("plottype should be equal to 'pval' or 'groups'.")
+
+        if (isTRUE(all.equal(plottype, "pval"))) {
+            df <- cbind(tab, kstestlog10 = -log10(tab[, pvalkstestcolname]))
+            kstestlog10str <- "kstestlog10"
+            df <- df %>% dplyr::arrange(df[, kstestlog10str])
+            aesvar <- ggplot2::aes(!!sym(auc_ctrlname), !!sym(auc_stressname), # nolint
+             color = !!sym(kstestlog10str))
+            geompointinfo <- ggplot2::geom_point(size = 0.5)
+            geompointinfo2 <- ggplot2::geom_density_2d()
+        } else {
+            df <- tab %>% dplyr::filter(!!sym(universename)) # nolint
+            dfatt <- tab %>% dplyr::filter(!!sym(groupname) == "Attenuated") # nolint
+            dfoutgroup <- tab %>% filter(!!sym(groupname) == "Outgroup") # nolint
+
+            aesvar <- ggplot2::aes(!!sym(auc_ctrlname), !!sym(auc_stressname)) # nolint
+            geompointinfo <- ggplot2::geom_point(size = 0.5, color = "grey")
+            geompointinfo2 <- ggplot2::geom_point(data = dfatt, aesvar,
+                color = "#e76f51", size = 1)
+            geompointinfo3 <- ggplot2::geom_point(data = dfoutgroup, aesvar,
+                    color = "#e9c46a", size = 1)
+        }
+
+        ## Structure of the basic scatterplot
+        g <- ggplot2::ggplot(df, aesvar) + geompointinfo + geompointinfo2
+
+        if (isTRUE(all.equal(plottype, "pval"))) {
+            ## Adding highlight of the genes
+            g <- g + ggrepel::geom_label_repel(data = subset(df,
+                gene %in% genevec), aes(label = gene), box.padding = 0.55, # nolint
+                point.padding = 0, segment.color = "black", max.overlaps = 50,
+                color = "red") +
+                ggplot2::scale_color_gradient2(midpoint = 0, low = "white",
+                    mid = "grey", high = "darkgreen")
+        } else {
+            g <- g + geompointinfo3
+        }
+
+        ## Formatting functions
+        g <- g + ggplot2::xlim(axismin_x, axismax_x) +
+            ggplot2::ylim(axismin_y, axismax_y) +
+            ggplot2::labs(x = labelx, y = labely, legend = "-log10 p-value",
+                color = "-log10 p-value", title = maintitle,
+                subtitle = subtitle) +
+            ggplot2::coord_fixed(ratio = 1) + ggplot2::theme_classic() +
+            ggplot2::theme(legend.position = legendpos)
+
+        if (plot) {
+            warning("You chose to plot the auc, the figure is not saved.")
+            print(g)
+        } else {
+            ggplot2::ggsave(filename = paste0(outfile, ".", formatname),
+                plot = g, device = formatname, path = outfold)
+        }
+}
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+plotauc(unigroupdf, plot = TRUE)
