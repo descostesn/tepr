7f244932d1ee8184d0f6ef7ce9b50352904c3e90
descoste <nicolas.descostes@embl.it>
2024-09-30 10:19:28 +0200

    initial commit

---
+++ R/utils.R
@@ -0,0 +1,14 @@
+.extractstr <- function(transtable) {
+
+    str <- as.character(unique(transtable$strand))
+    .checkunique(str, "str")
+    if (isTRUE(all.equal(str, "+"))) {
+        str <- "plus"
+    } else if (isTRUE(all.equal(str, "-"))) {
+        str <- "minus"
+    } else {
+        stop("In .computeecdf, strand is neither plus or minus. This ",
+            "should not happen. Contact the developer.")
+    }
+    return(str)
+}
