02d4f4e28b620e0491e4a3806e08a0c845f8ae95
descoste <nicolas.descostes@embl.it>
2024-10-01 12:23:07 +0200

    add rlang::.data

--- R/genesECDF.R
+++ R/genesECDF.R
@@ -34,7 +34,8 @@
             tidyr::gather(key = "variable", value = "value", colscorevec))
         dflong[, "value_round"] <- round(dflong$value * rounding)
         ecdflist <- lapply(unique(dflong$variable), function(currentvar) {
-            dfsubset <- subset(dflong, subset = variable == currentvar) # nolint
+            dfsubset <- subset(dflong,
+              subset = rlang::.data$variable == currentvar)
             dfexpanded <- dfsubset[rep(seq_len(nrow(dfsubset)),
                 dfsubset$value_round), ]
             funecdf <- ecdf(dfexpanded[, "coord"])
@@ -44,10 +45,10 @@
         resecdf <- dplyr::bind_rows(ecdflist)
 
         ## Shrink the results back to the transtable keeping ecdf columns
-        res <- resecdf %>% tidyr::pivot_wider(.,
+        res <- resecdf %>% tidyr::pivot_wider(rlang::.data,
             names_from = "variable",
             values_from = c("value", "value_round", "Fx")) %>%
-            dplyr::select(., -tidyselect::contains("value_round"))
+            dplyr::select(rlang::.data, -tidyselect::contains("value_round"))
 
         ## Removing strand from column names
         res <- res %>% dplyr::rename_with(~gsub(paste0(".", str), "", .),
@@ -105,7 +106,8 @@
 #' @importFrom dplyr bind_rows
 #' @importFrom tidyr fill pivot_wider
 #' @importFrom tidyselect contains
-#'
+#' @importFrom rlang .data
+#' 
 #' @seealso
 #' [averageandfilterexprs]
 #' @export
