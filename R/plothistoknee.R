805b80394a0afcd057b4d2604483e4cd4e4c1f8f
descoste <nicolas.descostes@embl.it>
2024-10-01 10:23:50 +0200

    add doc

--- R/plothistoknee.R
+++ R/plothistoknee.R
@@ -1,3 +1,55 @@
+#' Plot Histogram of Distance from TSS to Knee Point
+#'
+#' This function generates a histogram showing the distribution of the distance
+#' from the transcription start site (TSS) to the knee point for attenuated
+#' genes. The distance can be plotted either as a percentage of the gene length
+#' or in kilobases (kb).
+#'
+#' @param unigroupdf A data frame containing gene-level statistics, including
+#'  knee point data and group classification (see universegroup).
+#' @param plottype A string specifying the type of distance to plot. Options are
+#'  \code{"percent"} for the percentage of the gene or \code{"kb"} for distance
+#'  in kilobases. Default is \code{"percent"}.
+#' @param xlimvec A numeric vector of length 2 specifying the limits of the
+#'  x-axis. Default is \code{NA}, which automatically sets the limits based on
+#'  \code{plottype}.
+#' @param binwidthval A numeric value for the width of the bins in the
+#'  histogram. Default is \code{NA}, which automatically selects a bin width
+#'  based on \code{plottype}.
+#' @param kneename A string specifying the name of the column in
+#'  \code{unigroupdf} that contains the knee point data. Default is
+#'  \code{"knee_AUC_HS"}.
+#' @param plot A logical flag indicating whether to display the plot
+#'  interactively (\code{TRUE}) or save it to a file (\code{FALSE}). Default is
+#'  \code{FALSE}.
+#' @param outfold A string specifying the output folder where the plot will be
+#'  saved if \code{plot = FALSE}. Default is the current directory.
+#' @param formatname A string specifying the format of the saved plot file.
+#'  Default is \code{"pdf"}.
+#' @param universename A string specifying the name of the column in
+#'  \code{unigroupdf} that defines the universe of genes. Default is
+#'  \code{"Universe"}.
+#' @param groupname A string specifying the name of the column in
+#'  \code{unigroupdf} that defines the group classification of genes. Default is
+#'  \code{"Group"}.
+#'
+#' @return A histogram showing the distribution of the distance from the TSS to
+#'  the knee point for attenuated genes. The plot can either be displayed
+#'  interactively or saved to a file.
+#'
+#' @examples
+#' # Assuming `unigroupdf` contains the necessary data:
+#' # plothistoknee(unigroupdf, plottype = "kb", xlimvec = c(0, 300),
+#' # binwidthval = 10)
+#'
+#' @seealso
+#' [universegroup]
+#'
+#' @importFrom ggplot2 ggplot aes geom_histogram xlab xlim labs theme_classic ggsave
+#' @importFrom dplyr filter
+#'
+#' @export
+
 plothistoknee <- function(unigroupdf, plottype = "percent", xlimvec = NA, # nolint
     binwidthval = NA, kneename = "knee_AUC_HS", plot = FALSE, outfold = "./",
     formatname = "pdf", universename = "Universe", groupname = "Group") {
