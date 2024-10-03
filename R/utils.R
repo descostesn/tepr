fe3488dae92487315a70dfb235e3bc5520af4125
descoste <nicolas.descostes@embl.it>
2024-10-03 18:09:54 +0200

    code until joineddflist

--- R/utils.R
+++ R/utils.R
@@ -27,3 +27,58 @@
                     "provided table.")
         }, tab))
 }
+
+
+
+
+
+library(dplyr)
+library(purrr)
+
+workingdir <- "bedgraph255"
+outprotcod <- "protCoding_dTAG_Cugusi_stranded_20230810.tsv"
+outlncrna <- "lncRNA_dTAG_Cugusi_stranded_20230810.tsv"
+outtsv <- "dTAG_Cugusi_stranded_20230810.tsv"
+window <- 200
+!protscoredir <- "protein_coding_score"
+!lncscoredir <- "lncRNA_score"
+bgpattern <- "*.bg"
+
+## Retrieving all bedgraph files
+if (verbose) message("Retrieving all bedgraph file paths")
+bedgraphfiles <- list.files(workingdir, pattern = bgpattern, full.names = TRUE)
+
+## Defining vectors for protein-coding and lncRNA files
+scoredirvec <- c(protscoredir, lncscoredir)
+
+joineddflist <- lappy(scoredirvec, function(scoredir, bedgraphfiles, window) {
+
+    files <- bedgraphfiles %>% purrr::map(~{
+        filename <- tools::file_path_sans_ext(basename(.))
+        file.path(scoredir, paste0(filename, ".window", window,
+            ".MANE.wmean.name.score"))
+    })
+    # reading all the files
+    colnamevec <- c("biotype", "chr", "coor1", "coor2", "transcript", "gene",
+        "strand", "window", "id", "dataset", "score")
+    dflist <- lapply(files, read.delim, header = FALSE, sep = "\t",
+        na.strings = "NAN", dec = ".", col.names = colnamevec,
+        stringsAsFactors = FALSE)
+
+    # joining all the files
+    joincolvec <- c("biotype" , "chr", "coor1", "coor2", "transcript", "gene",
+        "strand", "window", "id")
+    ## the last filter remove the PAR genes (pseudoautosomal genes both in X and Y)
+    joineddf <- purrr::reduce(dflist, dplyr::left_join, by = joincolvec) %>%
+        dplyr::filter(strand != "Y")
+
+    return(joineddf)
+
+}, bedgraphfiles, window)
+
+
+
+! write.table(joineddf, file = write_file_protein_coding, sep = "\t", row.names = FALSE, col.names = FALSE, quote = F)
+! write.table(joineddf, file = write_file_lncRNA, sep = "\t", row.names = FALSE, col.names = FALSE, quote = F)
+ rm(dflist)
+ rm(joineddf)
