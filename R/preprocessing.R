b5c48a9815b2dbe3fd1d190f36dae9400f2db75d
descoste <nicolas.descostes@embl.it>
2024-11-26 14:24:48 +0100

    Reading gencode file

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,10 +1,16 @@
-retrieveanno <- function(exptabpath, saveobjectpath = NA) {
+retrieveanno <- function(exptabpath, gencodepath, saveobjectpath = NA,
+    verbose = TRUE) {
 
     if (!is.na(saveobjectpath) && !file.exists(saveobjectpath))
         dir.create(saveobjectpath, recursive = TRUE)
 
     ## Reading the information about experiments
+    if (verbose) message("Reading the information about experiments")
     exptab <- read.csv(exptabpath, header = TRUE)
-    checkexptab(exptab)
+    checkexptab(exptab) # nolint
+
+    ## Reading gencode file
+    if (verbose) message("Reading gencode file")
+    gencode <- read.delim(gencodepath, header = FALSE, skip = 5)
 
 }
