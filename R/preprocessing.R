8c88707949e6e39d4bad64d066c41903f169a70d
descoste <nicolas.descostes@embl.it>
2024-11-26 14:26:45 +0100

    Keeping "transcript" annotations

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -10,7 +10,12 @@ retrieveanno <- function(exptabpath, gencodepath, saveobjectpath = NA,
     checkexptab(exptab) # nolint
 
     ## Reading gencode file
-    if (verbose) message("Reading gencode file")
+    if (verbose) message("Reading gencode file and filtering")
     gencode <- read.delim(gencodepath, header = FALSE, skip = 5)
+
+    ## Keeping "transcript" annotations
+    if (verbose) message("\t Keeping 'transcript' annotations")
+    gencode <- gencode[which(gencode$V3 == "transcript"), ]
+
 
 }
