bcad15e63fbf09fab2508a17e67da4159489759b
descoste <nicolas.descostes@embl.it>
2024-11-26 14:30:28 +0100

    Selecting Ensembl_canonical transcripts

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,3 +1,17 @@
+## For different strings provided in the vector "valvec", perform the filtering
+## of gentab on each string using the result of the previous filtering
+.grepsequential <- function(valvec, gentab, invert = FALSE, verbose = FALSE) {
+    invisible(sapply(valvec, function(val) {
+        idx <- grep(val, gentab$V9, invert = invert)
+        if (verbose)
+            message(val, " - ", length(idx), " gentab - ", nrow(gentab))
+        if (!isTRUE(all.equal(length(idx), 0)))
+            gentab <<- gentab[idx, ] ## This line enables sequential grep
+    }))
+    return(gentab)
+}
+
+
 retrieveanno <- function(exptabpath, gencodepath, saveobjectpath = NA,
     verbose = TRUE) {
 
@@ -16,6 +30,14 @@ retrieveanno <- function(exptabpath, gencodepath, saveobjectpath = NA,
     ## Keeping "transcript" annotations
     if (verbose) message("\t Keeping 'transcript' annotations")
     gencode <- gencode[which(gencode$V3 == "transcript"), ]
+
+    ## Selecting Ensembl_canonical transcripts i.e. most representative
+    ## transcript of the protein coding gene. This will be the MANE_Select
+    ## transcript if there is one, or a transcript chosen by an Ensembl
+    ## algorithm otherwise.
+    if (verbose) message("Selecting Ensembl_canonical transcripts")
+    gencodeprotcod <- .grepsequential("MANE_Select", gencode)
+
 
 
 }
