2442a7bd6557240abd12194a329c6f055320b2a7
descoste <nicolas.descostes@embl.it>
2024-11-28 16:20:57 +0100

    initial commit

---
+++ vignettes/sandbox_toremove/testpreprocessing.R
@@ -0,0 +1,16 @@
+
+gencodepath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/gencode.v43.basic.annotation.gtf" # nolint
+exptabpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/exptab-bedgraph.csv" # nolint
+finaltabpath <- "/g/romebioinfo/tmp/preprocessing"
+finaltabname <- "cugusi.tsv"
+saveobjectpath <- outputfolder
+
+windsize <- 200
+blacklistshpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/hg38-blacklist.v2.bed" # nolint
+maptrackpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/k50.umap.hg38.0.8.bed" # nolint
+nbcpubg <- 8
+nbcputrans <- 20
+
+finaltab <- preprocessing(exptabpath, gencodepath, windsize, maptrackpath,
+    blacklistshpath, nbcputrans, nbcpubg, finaltabpath, finaltabname,
+    saveobjectpath)
