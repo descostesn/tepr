705852f82f41d24549e8180729358f8a4e94a662
descoste <nicolas.descostes@embl.it>
2024-11-28 15:22:55 +0100

    add makewindows

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,10 +1,17 @@
-preprocessing <- function(exptabpath, gencodepath, saveobjectpath = NA,
-    verbose = TRUE) {
+preprocessing <- function(exptabpath, gencodepath, windsize, nbcputrans = 1,
+    saveobjectpath = NA, verbose = TRUE) {
 
     ## This function filters gencode annotations to retrieve "transcript". It
     ## then distinguishes transcripts coming from protein coding genes
     ## (MANE_Select) and those coming from long non-coding genes (lncRNA,
     ## Ensembl_canonical).
     if (verbose) message("## Filtering gencode annotations\n")
-    anno <- retrieveanno(exptabpath, gencodepath, saveobjectpath, verbose)
+    allannobed <- retrieveanno(exptabpath, gencodepath, saveobjectpath, verbose)
+
+    ## This functions uses the annotations filtered from gencode (allannobed).
+    ## It removes any ensembl names containing "PAR_Y", filters out intervals
+    ## smaller than windsize and splits each transcript into "windsize" windows.
+    if (verbose) message("\n ## Splitting transcripts into windows\n")
+    allwindowsbed <- makewindows(allannobed, windsize, nbcputrans, verbose,
+        saveobjectpath)
 }
