9743d9e62b2adb0d4363ab916f7f518b307e9c19
descoste <nicolas.descostes@embl.it>
2024-11-29 08:29:34 +0100

    correct path

--- vignettes/sandbox_toremove/testpreprocessing.R
+++ vignettes/sandbox_toremove/testpreprocessing.R
@@ -3,7 +3,7 @@ gencodepath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/gencode.v43.b
 exptabpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/exptab-bedgraph.csv" # nolint
 finaltabpath <- "/g/romebioinfo/tmp/preprocessing"
 finaltabname <- "cugusi.tsv"
-saveobjectpath <- outputfolder
+saveobjectpath <- finaltabpath
 
 windsize <- 200
 blacklistshpath <- "/g/romebioinfo/Projects/tepr/downloads/annotations/hg38-blacklist.v2.bed" # nolint
