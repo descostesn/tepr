4eeeae21677a4a99dae6d3c4d06a2d1d85da5c6e
descoste <nicolas.descostes@embl.it>
2024-12-06 14:38:34 +0100

    set showtime = FALSE

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,7 +1,7 @@
 preprocessing <- function(exptabpath, gencodepath, windsize, maptrackpath,
     blacklistshpath, nbcputrans = 1, nbcpubg = 1, finaltabpath = "./",
     finaltabname = "anno.tsv", saveobjectpath = NA, savefinaltable = TRUE,
-    reload = FALSE, showstats = FALSE, showtime = TRUE, verbose = TRUE) {
+    reload = FALSE, showstats = FALSE, showtime = FALSE, verbose = TRUE) {
 
     ## This function filters gencode annotations to retrieve "transcript". It
     ## then distinguishes transcripts coming from protein coding genes
