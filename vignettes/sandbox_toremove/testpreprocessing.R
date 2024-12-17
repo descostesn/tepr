f246ea92f1cc5d5631ef2b2245fb4bd742d9b43a
descoste <nicolas.descostes@embl.it>
2024-12-17 14:00:49 +0100

    adapt call

--- vignettes/sandbox_toremove/testpreprocessing.R
+++ vignettes/sandbox_toremove/testpreprocessing.R
@@ -15,8 +15,8 @@ saveobjectpath <- finaltabpath
 windsize <- 200
 blacklistshpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/hg38-blacklist.v2.bed" # nolint
 maptrackpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/k50.umap.hg38.0.8.bed" # nolint
-#nbcpubg <- 8
-nbcputrans <- 20
+nbcpubg <- 8
+nbcputrans <- 15
 reload <- TRUE
 verbose <- TRUE
 showtime <- TRUE
@@ -34,7 +34,6 @@ source("/g/romebioinfo/Projects/tepr/R/preprocessing-createtablescores.R")
 source("/g/romebioinfo/Projects/tepr/R/preprocessing.R")
 source("/g/romebioinfo/Projects/tepr/R/utils.R")
 
-preprocessing(exptabpath, gencodepath, windsize, maptrackpath,
-    blacklistshpath, genomename, nbcputrans, nbcpubg,
-    finaltabpath, finaltabname, saveobjectpath,
-    savefinaltable, reload, showtime, verbose)
+preprocessing(exptabpath, gencodepath, windsize, maptrackpath, blacklistshpath,
+    genomename, nbcputrans, nbcpubg, finaltabpath, finaltabname, tmpfold,
+    saveobjectpath, savefinaltable, reload, showtime, verbose)
