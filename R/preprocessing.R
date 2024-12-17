5e51b3029d549219e39271c31dde49a4e6340aff
descoste <nicolas.descostes@embl.it>
2024-12-17 13:55:32 +0100

    remove doc

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -42,83 +42,9 @@
                 tmpfold, reload, showtime, verbose)
 }
 
-#' Preprocess Genomic Data
-#'
-#' @description
-#' This function preprocesses genomic data by filtering, splitting, and
-#' annotating transcripts based on the given parameters. It generates a final
-#' annotated table.
-#'
-#' @usage
-#' preprocessing(exptabpath, gencodepath, windsize, maptrackpath,
-#' blacklistshpath, nbcputrans = 1, nbcpubg = 1, finaltabpath = "./",
-#'     finaltabname = "anno.tsv", saveobjectpath = NA, savefinaltable = TRUE,
-#'     reload = FALSE, showtime = FALSE, verbose = TRUE)
-#'
-#' @param exptabpath Path to the experiment table file containing a table with
-#'              columns named 'condition', 'replicate', 'strand', and 'path'.
-#' @param gencodepath Path to the GENCODE annotation file.
-#' @param windsize Window size for splitting transcripts into intervals.
-#' @param maptrackpath Path to the mappability track file.
-#' @param blacklistshpath Path to the blacklist file.
-#' @param nbcputrans Number of CPU cores to use for transcript-level operations.
-#' @param nbcpubg Number of CPU cores to use for bedgraph-level operations.
-#' @param finaltabpath Directory path to save the final annotated table.
-#' @param finaltabname Name of the final annotated table file.
-#' @param saveobjectpath Path to save intermediate R objects. Default is `NA`
-#'  and R objects are not saved.
-#' @param savefinaltable Logical. If `TRUE`, saves the final table as TSV to
-#'  disk. Default is `TRUE`.
-#' @param reload Logical. If `TRUE`, reloads existing saved objects to avoid
-#'  recomputation. Default is `FALSE`. If the function failed during object
-#'  saving, make sure to delete the corresponding object.
-#' @param showtime Logical. If `TRUE`, displays timing information. Default is
-#'  `FALSE`.
-#' @param verbose Logical. If `TRUE`, provides detailed messages during
-#'  execution. Default is `TRUE`.
-#'
-#' @return A data frame containing the final annotated table.
-#'
-#' @details
-#' The `preprocessing` function performs the following steps:
-#'
-#' 1. Filters GENCODE annotations to retrieve "transcript" entries and
-#'  categorizes them as protein-coding or long non-coding RNA.
-#' 2. Splits transcripts into windows of size `windsize`, filtering out
-#'  intervals smaller than this size and removing entries with "PAR_Y" in their
-#'  Ensembl names.
-#' 3. Retrieves values from bedgraph files, applies blacklist filtering, and
-#'  retains scores in high-mappability regions.
-#' 4. Merges results into a final table and optionally saves it to disk.
-#'
-#' If `reload` is set to `TRUE` and saved objects exist, the function avoids
-#' recomputation by reusing those objects.
-#'
-#' @examples
-#' # Example usage:
-#' preprocessing(
-#'   exptabpath = "experiment_table.tsv",
-#'   gencodepath = "gencode.v37.annotation.gtf",
-#'   windsize = 200,
-#'   maptrackpath = "mappability.bedgraph",
-#'   blacklistshpath = "blacklist.bed",
-#'   finaltabpath = "./results",
-#'   finaltabname = "annotated_results.tsv",
-#'   saveobjectpath = "./intermediate_objects",
-#'   savefinaltable = TRUE,
-#'   reload = FALSE,
-#'   showtime = TRUE,
-#'   verbose = TRUE
-#' )
-#'
-#' @importFrom tibble as_tibble add_column
-#' @importFrom rtracklayer import.bedGraph
-#' @importFrom dplyr relocate filter full_join
-#' @importFrom valr bed_intersect
-#' @importFrom parallel mclapply makeCluster parLapply stopCluster
-#' @importFrom purrr reduce map2
-#'
-#' @export
+# !!!!!!
+# !!!!!! DOC TO DO
+# !!!!!!
 
 preprocessing <- function(exptabpath, gencodepath, windsize, maptrackpath,
     blacklistshpath, genomename, nbcputrans = 1, nbcpubg = 1,
