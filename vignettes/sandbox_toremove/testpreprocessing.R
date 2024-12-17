e98397b80decdbc3c0319bffd2a6d63b7224571c
descoste <nicolas.descostes@embl.it>
2024-12-17 08:32:31 +0100

    change nb cpu

--- vignettes/sandbox_toremove/testpreprocessing.R
+++ vignettes/sandbox_toremove/testpreprocessing.R
@@ -15,7 +15,7 @@ saveobjectpath <- finaltabpath
 windsize <- 200
 blacklistshpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/hg38-blacklist.v2.bed" # nolint
 maptrackpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/k50.umap.hg38.0.8.bed" # nolint
-nbcpubg <- 8
+#nbcpubg <- 8
 nbcputrans <- 20
 reload <- TRUE
 verbose <- TRUE
