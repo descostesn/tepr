9fafa5fbafbf177dc92134643294984b3eb1a9e8
descoste <nicolas.descostes@embl.it>
2024-12-19 17:25:04 +0100

    format timing

--- R/universegroup.R
+++ R/universegroup.R
@@ -119,7 +119,8 @@ universegroup <- function(completedf, controlname = "ctrl", stressname = "HS", #
 
     if (showtime) {
       end_time <- Sys.time()
-      message("\t\t ## Analysis performed in: ", end_time - start_time) # nolint
+      timing <- end_time - start_time
+      message("\t\t ## Analysis performed in: ", format(timing, digits = 2))
     }
 
     return(completedf)
