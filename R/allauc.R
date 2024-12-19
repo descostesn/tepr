3d1ff69630ca406e8d4db9feb6c869e8b5dff264
descoste <nicolas.descostes@embl.it>
2024-12-19 17:14:22 +0100

    format timing

--- R/allauc.R
+++ R/allauc.R
@@ -216,7 +216,8 @@ allauc <- function(bytranslistmean, expdf, nbwindows, nbcpu = 1,
 
     if (showtime) {
       end_time <- Sys.time()
-      message("\t\t ## Analysis performed in: ", end_time - start_time) # nolint
+      timing <- end_time - start_time
+      message("\t\t ## Analysis performed in: ", format(timing, digits = 2))
     }
     return(allauc)
 }
