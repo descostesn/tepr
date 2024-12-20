6dd3bcab12c5aa32a5d5786aa77bb4749b2ae769
descoste <nicolas.descostes@embl.it>
2024-12-20 14:45:33 +0100

    test 5 cpu

--- vignettes/sandbox_toremove/testpreprocessing.R
+++ vignettes/sandbox_toremove/testpreprocessing.R
@@ -8,21 +8,21 @@ library("tidyverse")
 
 gencodepath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/gencode.v43.basic.annotation.gtf" # nolint
 exptabpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/exptab-bedgraph-vicnames.csv" # nolint
-finaltabpath <- "/g/romebioinfo/tmp/preprocessing/objects-tsv-7cpus"
+finaltabpath <- "/g/romebioinfo/tmp/preprocessing/objects-tsv-5cpus"
 finaltabname <- "cugusi.tsv"
 saveobjectpath <- finaltabpath
 
 windsize <- 200
 blacklistshpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/hg38-blacklist.v2.bed" # nolint
 maptrackpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/k50.umap.hg38.0.8.bed" # nolint
-nbcpubg <- 8
+nbcpubg <- 5
 nbcputrans <- 7
 reload <- TRUE
 verbose <- TRUE
 showtime <- TRUE
 showmemory <- TRUE
 genomename <- "hg38"
-tmpfold <- "./tmp-7cpu"
+tmpfold <- "./tmp-5cpu"
 savefinaltable <- TRUE
 # nbcpubg <- 1
 # nbcputrans <- 1
