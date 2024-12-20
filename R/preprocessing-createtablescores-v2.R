9a3b716adb83d5b47a22c6822e450704b8ec5631
descoste <nicolas.descostes@embl.it>
2024-12-20 14:37:16 +0100

    add folder creation

--- R/preprocessing-createtablescores-v2.R
+++ R/preprocessing-createtablescores-v2.R
@@ -140,7 +140,9 @@ createtablescores <- function(tmpfold, exptabpath, showmemory = FALSE,
             finaltab[, idxdataset] <- rep(expname, nbrows)
         }
 
-        if (savefinaltable) {
+        if (savefinaltable) {`
+            if (!file.exists(finaltabpath))
+                dir.create(finaltabpath, recursive = TRUE)
             outfile <- file.path(finaltabpath, finaltabname)
             if (verbose) message("\n ## Saving the final table to ", outfile)
             write.table(finaltab, file = outfile, sep = "\t", quote = FALSE,
