90320d824db7f9008375183b1cfc1e63b2e9982e
descoste <nicolas.descostes@embl.it>
2024-12-22 09:58:17 +0100

    test 3 cpus

--- vignettes/sandbox_toremove/cluster.sh
+++ vignettes/sandbox_toremove/cluster.sh
@@ -2,10 +2,10 @@
 #SBATCH --array=1
 #SBATCH --nodes=1
 #SBATCH --mem=60gb
-#SBATCH --time=12:00:00
+#SBATCH --time=15:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
-#SBATCH --job-name=bgmean5cpu
+#SBATCH --cpus-per-task=3
+#SBATCH --job-name=bgmean3cpu
 #SBATCH --output=slurm_%x_%A_%a.out
 
 module purge
