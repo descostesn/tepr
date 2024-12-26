651085c8c2f35bf55f878df6d511844317fd642b
descoste <nicolas.descostes@embl.it>
2024-12-26 12:14:27 +0100

    add countna

--- R/tepr.R
+++ R/tepr.R
@@ -1,6 +1,15 @@
-tepr <- function(expdf, alldf, expthres, showtime = FALSE, verbose = TRUE) {
+tepr <- function(expdf, alldf, expthres, nbcpu = 1, showtime = FALSE,
+    verbose = TRUE) {
 
+    ## This function calculates the average expression levels for transcripts
+    ## from a provided expression data frame and filters out transcripts based
+    ## on a specified expression threshold.
     resallexprs <- averageandfilterexprs(expdf, alldf, expthres, showtime,
         verbose = TRUE)
+
+    ## This function takes a list of expression data frames, a condition
+    ## information data frame, and counts the number of NA values for each
+    # transcript based on strand and condition.
+    rescountna <- countna(resallexprs, expdf, nbcpu, showtime, verbose)
 
 }
