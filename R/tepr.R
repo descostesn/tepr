55a085db6f69dd01bf558a5454d9c07d68ec5c73
descoste <nicolas.descostes@embl.it>
2024-12-26 12:08:33 +0100

    import averageandfilterexprs

---
+++ R/tepr.R
@@ -0,0 +1,6 @@
+tepr <- function(expdf, alldf, expthres, showtime = FALSE, verbose = TRUE) {
+
+    resallexprs <- averageandfilterexprs(expdf, alldf, expthres, showtime,
+        verbose = TRUE)
+
+}
