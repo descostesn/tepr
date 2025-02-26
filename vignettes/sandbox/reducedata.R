3f421752804eaf505d162f9cb4aa3a8af05e595b
descoste <nicolas.descostes@embl.it>
2025-02-26 15:40:19 +0100

    test with new files

--- vignettes/sandbox/reducedata.R
+++ vignettes/sandbox/reducedata.R
@@ -1,5 +1,5 @@
-exptabpath = "newfilesreduced/exptab_limited.csv"
-gencodepath = "newfilesreduced/gencode-chr13_limited.gtf"
+exptabpath = "exptab_limited.csv"
+gencodepath = "gencode-chr13_limited.gtf"
 windsize = 200
 maptrackpath = "k50.umap.chr13.hg38.0.8.bed"
 blacklistpath = "hg38-blacklist-chr13.v2.bed"
@@ -8,6 +8,8 @@ nbcputrans = 5; finaltabpath = "./"; finaltabname = "anno.tsv"
 tmpfold = "./tmp"; saveobjectpath = NA; savefinaltable = TRUE;
 reload = FALSE; showtime = TRUE; showmemory = TRUE; deletetmp = TRUE
 chromtab = NA; verbose = TRUE
+
+
 
 ## makewindows
 expbed = allannobed; nbwindows = windsize
