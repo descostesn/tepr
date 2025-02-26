3522dc9491a990ec54f99cd6386511b685f0a5fe
descoste <nicolas.descostes@embl.it>
2025-02-26 09:47:32 +0100

    initial commit

---
+++ vignettes/sandbox/reducedata.R
@@ -0,0 +1,10 @@
+exptabpath = "exptab.csv"
+gencodepath = "gencode-chr13.gtf"
+windsize = 200
+maptrackpath = "k50.umap.chr13.hg38.0.8.bed"
+blacklistshpath = "hg38-blacklist-chr13.v2.bed"
+genomename = "hg38"
+nbcputrans = 5; finaltabpath = "./"; finaltabname = "anno.tsv"
+tmpfold = "./tmp"; saveobjectpath = NA; savefinaltable = TRUE;
+reload = FALSE; showtime = TRUE; showmemory = TRUE; deletetmp = TRUE
+chromtab = NA; verbose = TRUE
