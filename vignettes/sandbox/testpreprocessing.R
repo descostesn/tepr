4e327a799772c2eaab57b8043061bb49f06862b2
descoste <nicolas.descostes@embl.it>
2025-03-12 11:22:52 +0100

    initial commit

---
+++ vignettes/sandbox/testpreprocessing.R
@@ -0,0 +1,28 @@
+library("tepr")
+
+gencodepath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/gencode.v43.basic.annotation.gtf" # nolint
+exptabpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/exptab-bedgraph-vicnames.csv" # nolint
+finaltabpath <- "objects-tsv-10cpus"
+finaltabname <- "cugusi.tsv"
+saveobjectpath <- finaltabpath
+
+windsize <- 200
+blacklistshpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/hg38-blacklist.v2.bed" # nolint
+maptrackpath <- "/g/romebioinfo/Projects/tepr-data/downloads/annotations/k50.umap.hg38.0.8.bed" # nolint
+nbcputrans <- 10
+reload <- TRUE
+verbose <- TRUE
+showtime <- TRUE
+showmemory <- TRUE
+genomename <- "hg38"
+tmpfold <- "tmp-10cpu"
+savefinaltable <- TRUE
+deletetmp <- FALSE
+chromtest <- rtracklayer::SeqinfoForUCSCGenome(genomename)
+
+finaltable <- preprocessing(exptabpath, gencodepath, windsize, maptrackpath,
+    blacklistshpath, genomename, nbcputrans, finaltabpath,
+    finaltabname, tmpfold, saveobjectpath,
+    savefinaltable, reload, showtime, showmemory,
+    deletetmp, chromtab = chromtest, verbose)
+
