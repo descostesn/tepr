0dde9d8764a3fff6abc42ddac967f300a27bef25
descoste <nicolas.descostes@embl.it>
2025-03-14 13:01:50 +0100

    done

--- tests/testthat/test-preprocessing-makewindows.R
+++ tests/testthat/test-preprocessing-makewindows.R
@@ -4,10 +4,10 @@ gencodepath <- system.file("extdata", "gencode-chr13.gtf", package = "tepr")
 windsize <- 200
 
 ## Necessary result to call makewindows
-allannobed <- retrieveanno(exptabpath, gencodepath)
+allannobed <- retrieveanno(exptabpath, gencodepath, verbose = FALSE)
 
 ## Calling the function to test
-allwindowsbed <- makewindows(allannobed, windsize)
+allwindowsbed <- makewindows(allannobed, windsize, verbose = FALSE)
 
 ## ---- Comparing to expected object ---- ##
 expectedobj <- readRDS(system.file("extdata", "allwindowsbed.rds",
@@ -15,5 +15,3 @@ expectedobj <- readRDS(system.file("extdata", "allwindowsbed.rds",
 test_that("makewindows works properly", {
              expect_identical(allwindowsbed, expectedobj)
          })
-
-## Generating error
