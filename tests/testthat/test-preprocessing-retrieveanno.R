145383d34b9709557cb0f7fd95e1a6f03d48f2e3
descoste <nicolas.descostes@embl.it>
2025-03-14 13:00:26 +0100

    initial commit

---
+++ tests/testthat/test-preprocessing-retrieveanno.R
@@ -0,0 +1,14 @@
+## Data
+exptabpath <- system.file("extdata", "exptab-preprocessing.csv", package="tepr")
+gencodepath <- system.file("extdata", "gencode-chr13.gtf", package = "tepr")
+
+## Call the function to test
+allannobed <- retrieveanno(exptabpath, gencodepath, verbose = FALSE)
+
+## ---- Comparing to expected object ---- ##
+expectedobj <- readRDS(system.file("extdata", "allannobed.rds",
+    package="tepr"))
+test_that("retrieveanno works properly", {
+             expect_identical(allannobed, expectedobj)
+         })
+
