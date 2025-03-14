b4b36fb7b4380f3d3b9ccc203afdea3fe7b07c51
descoste <nicolas.descostes@embl.it>
2025-03-14 17:56:20 +0100

    add task

--- tests/testthat/test-preprocessing-createtablescores.R
+++ tests/testthat/test-preprocessing-createtablescores.R
@@ -5,6 +5,7 @@ maptrackpath <- system.file("extdata", "k50.umap.chr13.hg38.0.8.bed",
     package = "tepr")
 blacklistpath <- system.file("extdata", "hg38-blacklist-chr13.v2.bed",
     package = "tepr")
+tmpfoldpath <- file.path(getwd(), "tmptepr")
 windsize <- 200
 genomename <- "hg38"
 chromtabtest <- rtracklayer::SeqinfoForUCSCGenome(genomename)
@@ -28,8 +29,8 @@ blacklisthighmap(maptrackpath, blacklistpath, exptabpath, nbcputrans = 1,
     verbose = FALSE)
 
 ## Calling the function to test
-finaltabtest <- createtablescores(tmpfold = file.path(getwd(), "tmptepr"),
-    exptabpath, savefinaltable = FALSE, verbose = FALSE)
+finaltabtest <- createtablescores(tmpfold = tmpfoldpath, exptabpath,
+    savefinaltable = FALSE, verbose = FALSE)
 
 
 ## ---- Comparing to expected object ---- ##
@@ -38,3 +39,20 @@ expectedobj <- readRDS(system.file("extdata", "finaltab.rds",
 test_that("createtablescores works properly", {
              expect_identical(finaltabtest, expectedobj)
          })
+
+## ----- Checking errors ----- ##
+
+test_that("Errors are thrown when calling createtablescores", {
+
+    write.table(data.frame(1:10), file = file.path(tmpfoldpath,
+    paste0(expdfpre[1, 1], expdfpre[1, 2], expdfpre[1, 3], "-chrtest.tsv")))
+    expm <- paste0("\n\t Experiments have a different number of files. This ",
+                "should not happen. Contact the developer.\n")
+
+})
+
+
+
+tmpfold = file.path(getwd(), "tmptepr")
+showmemory = FALSE; showtime = FALSE; savefinaltable = FALSE; finaltabpath = getwd()
+finaltabname = "anno.tsv"; verbose = TRUE
