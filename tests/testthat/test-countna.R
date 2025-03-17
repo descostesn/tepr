8643129bda19890dad5a158b0d34f1867e415d86
descoste <nicolas.descostes@embl.it>
2025-03-17 16:23:10 +0100

    initial commit

---
+++ tests/testthat/test-countna.R
@@ -0,0 +1,18 @@
+## Parameters
+exppath <-  system.file("extdata", "exptab.csv", package="tepr")
+transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
+expthres <- 0.1
+
+## Calculating averageandfilterexprs to call countNA
+expdf <- read.csv(exppath)
+transdf <- read.delim(transpath, header = FALSE)
+avfilttest <- averageandfilterexprs(expdf, transdf, expthres,
+        showtime = FALSE, verbose = FALSE)
+
+## ---- Comparing to expected object ---- ##
+expectedobj <- readRDS(system.file("extdata", "countna.rds",
+    package="tepr"))
+countnatest <- countna(avfilttest, expdf, nbcpu = 1, verbose = FALSE)
+test_that("countna works properly", {
+             expect_identical(countnatest, expectedobj)
+         })
