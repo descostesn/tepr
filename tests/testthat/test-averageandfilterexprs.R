0c7cff059a87e2597e4c2c7100c01e25dd685ff6
descoste <nicolas.descostes@embl.it>
2025-03-17 16:06:23 +0100

    second error

--- tests/testthat/test-averageandfilterexprs.R
+++ tests/testthat/test-averageandfilterexprs.R
@@ -1,11 +1,27 @@
 ## Parameters
 exppath <-  system.file("extdata", "exptab.csv", package="tepr")
 transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
-expthres <- 0.0000000000000000000000001
+expthres <- 0.1
 
 ## Read input tables
 expdf <- read.csv(exppath)
 transdf <- read.delim(transpath, header = FALSE)
 
-reslist <- averageandfilterexprs(expdf, transdf, expthres, showtime = FALSE, # nolint
-    verbose = TRUE)
+## ----- Checking errors ----- ##
+test_that("Errors are thrown when calling averageandfilterexprs", {
+
+    expthres <- 10000
+    expm <- paste0("\n No transcript was identified as expressed. You might ",
+                "want to decrease the expthres parameter. Currently all genes",
+                " whose expression < ", expthres, " are removed.\n")
+    expect_error(averageandfilterexprs(expdf, transdf, expthres,
+        showtime = FALSE, verbose = FALSE), regexp = expm)
+
+    transdf$V7[which(transdf$V7 == '+')] <- "toto"
+    expthres <- 0.1
+    expm <- paste0("\n\t The strand name is neither \\+ or \\- in the ",
+        "transcript table alldf. If you are sure to have built alldf with the ",
+        "preprocessing function, contact the developer.\n")
+    expect_error(averageandfilterexprs(expdf, transdf, expthres,
+        showtime = FALSE, verbose = FALSE), regexp = expm)
+})
