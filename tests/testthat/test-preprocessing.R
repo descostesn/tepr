2bf7fd2f3210004a41e9fd23e1b060cdfb29c497
descoste <nicolas.descostes@embl.it>
2025-03-17 12:05:22 +0100

    test on genome name

--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -1,13 +1,20 @@
 ## Data
-exptabpath <- system.file("extdata", "exptab-preprocessing.csv", package="tepr")
+exptabpath <- system.file("extdata", "exptab-preprocessing.csv", package = "tepr")
 gencodepath <- system.file("extdata", "gencode-chr13.gtf", package = "tepr")
 maptrackpath <- system.file("extdata", "k50.umap.chr13.hg38.0.8.bed",
-    package = "tepr")
+  package = "tepr")
 blacklistpath <- system.file("extdata", "hg38-blacklist-chr13.v2.bed",
     package = "tepr")
 windsize <- 200
 genomename <- "hg38"
 
+## Calling the function to test
+test_that("Errors are thrown when calling preprocessing", {
 
-preprocessing <- function(exptabpath, gencodepath, windsize, maptrackpath,
-    blacklistshpath, genomename)
+    expm <- "\n\t Either the genome name or chromtab should be provided.\n"
+    expect_error(preprocessing(exptabpath, gencodepath, windsize, maptrackpath,
+    blacklistshpath, genomename = NA), regexp = expm)
+
+})
+
+
