cb72b4dfea625e3eaa0e1b3cc07075cc7a692281
descoste <nicolas.descostes@embl.it>
2025-03-18 16:24:11 +0100

    remove countna

--- tests/testthat/test-allauc.R
+++ tests/testthat/test-allauc.R
@@ -8,7 +8,6 @@ expdf <- read.csv(exppath)
 transdf <- read.delim(transpath, header = FALSE)
 avfilt <- averageandfilterexprs(expdf, transdf, expthres,
         showtime = FALSE, verbose = FALSE)
-countna <- countna(avfilt, expdf, nbcpu = 1, verbose = FALSE)
 ecdf <- genesECDF(avfilt, expdf, verbose = FALSE)
 resecdf <- ecdf[[1]]
 nbwindows <- ecdf[[2]]
