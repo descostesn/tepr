d6ef7aa6b4949350d77b2253f59a13c38afaa4a5
descoste <nicolas.descostes@embl.it>
2025-03-18 18:17:00 +0100

    identity

---
+++ tests/testthat/test-attenuation.R
@@ -0,0 +1,28 @@
+## Parameters
+exppath <-  system.file("extdata", "exptab.csv", package="tepr")
+transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
+expthres <- 0.1
+
+## Calculating necessary results
+expdf <- read.csv(exppath)
+transdf <- read.delim(transpath, header = FALSE)
+avfilt <- averageandfilterexprs(expdf, transdf, expthres,
+        showtime = FALSE, verbose = FALSE)
+rescountna <- countna(avfilttest, expdf, nbcpu = 1, verbose = FALSE)
+ecdf <- genesECDF(avfilt, expdf, verbose = FALSE)
+resecdf <- ecdf[[1]]
+nbwindows <- ecdf[[2]]
+resmeandiff <- meandifference(resecdf, expdf, nbwindows,
+    verbose = FALSE)
+bytranslistmean <- split(resmeandiff, factor(resmeandiff$transcript))
+resknee <- kneeid(bytranslistmean, expdf, verbose = FALSE)
+resauc <- allauc(bytranslistmean, expdf, nbwindows, verbose = FALSE)
+
+## ---- Comparing to expected object ---- ##
+expectedobj <- readRDS(system.file("extdata", "attenuation.rds",
+    package="tepr"))
+resatt <- attenuation(resauc, resknee, rescountna, bytranslistmean, expdf,
+        resmeandiff, verbose = FALSE)
+test_that("attenuation works properly", {
+             expect_identical(resatt, expectedobj)
+         })
