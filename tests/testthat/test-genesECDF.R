c61fef3e857458b774c854d989d11af65f97a671
descoste <nicolas.descostes@embl.it>
2025-03-18 11:03:02 +0100

    initial commit

---
+++ tests/testthat/test-genesECDF.R
@@ -0,0 +1,19 @@
+## Parameters
+exppath <-  system.file("extdata", "exptab.csv", package="tepr")
+transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
+expthres <- 0.1
+
+## Calculating averageandfilterexprs and countNA to call genesECDF
+expdf <- read.csv(exppath)
+transdf <- read.delim(transpath, header = FALSE)
+avfilttest <- averageandfilterexprs(expdf, transdf, expthres,
+        showtime = FALSE, verbose = FALSE)
+countnatest <- countna(avfilttest, expdf, nbcpu = 1, verbose = FALSE)
+
+## ---- Comparing to expected object ---- ##
+expectedobj <- readRDS(system.file("extdata", "genesecdf.rds",
+    package="tepr"))
+ecdftest <- genesECDF(avfilttest, expdf, verbose = FALSE)
+test_that("countna works properly", {
+             expect_identical(ecdftest, expectedobj)
+         })
