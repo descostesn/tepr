79b1ace6d5ea6016357323601d15d721be5c773c
descoste <nicolas.descostes@embl.it>
2025-04-09 09:33:12 +0200

    update example

--- R/kneeid.R
+++ R/kneeid.R
@@ -44,9 +44,24 @@ reslist <- lapply(condvec, function(cond, transtable) {
 #'  condition.
 #'
 #' @examples
-#' # Assuming transdflist is a list of transcript data frames and expdf contains
-#' # conditions for each experiment:
-#' # result <- kneeid(transdflist, expdf, nbcpu = 4, verbose = TRUE)
+#' exppath <-  system.file("extdata", "exptab.csv", package="tepr")
+#' transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
+#' expthres <- 0.1
+#'
+#' ## Calculating necessary results
+#' expdf <- read.csv(exppath)
+#' transdf <- read.delim(transpath, header = FALSE)
+#' avfilt <- averageandfilterexprs(expdf, transdf, expthres,
+#'         showtime = FALSE, verbose = FALSE)
+#' ecdf <- genesECDF(avfilt, expdf, verbose = FALSE)
+#' resecdf <- ecdf[[1]]
+#' nbwindows <- ecdf[[2]]
+#' meandiff <- meandifference(resecdf, expdf, nbwindows,
+#'     verbose = FALSE)
+#' bytranslistmean <- split(meandiff, factor(meandiff$transcript))
+#'
+#' ## Testing kneeid
+#' reskneeid <- kneeid(bytranslistmean, expdf, verbose = FALSE)
 #'
 #' @importFrom parallel mclapply
 #' @importFrom dplyr slice_min
