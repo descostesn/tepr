0dbd5ea6d4dccba19d261744c8cba249296f3fc6
descoste <nicolas.descostes@embl.it>
2025-04-09 09:17:35 +0200

    format example

--- R/attenuation.R
+++ R/attenuation.R
@@ -133,8 +133,12 @@
 #' exppath <-  system.file("extdata", "exptab.csv", package="tepr")
 #' transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
 #' expthres <- 0.1
+#' 
+#' ## Reading tables
 #' expdf <- read.csv(exppath)
 #' transdf <- read.delim(transpath, header = FALSE)
+#' 
+#' ## Computing intermediate steps
 #' avfilt <- averageandfilterexprs(expdf, transdf, expthres,
 #'        showtime = FALSE, verbose = FALSE)
 #' rescountna <- countna(avfilt, expdf, nbcpu = 1, verbose = FALSE)
@@ -146,6 +150,8 @@
 #' bytranslistmean <- split(resmeandiff, factor(resmeandiff$transcript))
 #' resknee <- kneeid(bytranslistmean, expdf, verbose = FALSE)
 #' resauc <- allauc(bytranslistmean, expdf, nbwindows, verbose = FALSE)
+#' 
+#' ## Testing attenuation
 #' resatt <- attenuation(resauc, resknee, rescountna, bytranslistmean, expdf,
 #'         resmeandiff, verbose = FALSE)
 #'
