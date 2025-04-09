a0ed7b77763b34ddc848cc68892b4e8b31bff6e1
descoste <nicolas.descostes@embl.it>
2025-04-09 10:57:50 +0200

    update example

--- R/plotmulti.R
+++ R/plotmulti.R
@@ -210,7 +210,9 @@
 #' @examples
 #' # Assuming resteprmulti is the object returned by the function teprmulti
 #' # and expdf contains the necessary data:
-#' # plotmulti(resteprmulti, expdf, ecdfgenevec = c("EGFR", "DAP", "FLI1"))
+#' \donttest {
+#' plotmulti(resteprmulti, expdf, ecdfgenevec = c("EGFR", "DAP", "FLI1"))
+#' }
 #'
 #' @seealso
 #' [teprmulti], [plotecdf], [plotauc], [plotmetagenes], [plothistoknee]
