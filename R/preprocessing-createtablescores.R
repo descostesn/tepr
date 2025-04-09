d62e6122aa9fa8195d3287442a1272db794090ca
descoste <nicolas.descostes@embl.it>
2025-04-09 11:10:02 +0200

    update example

--- R/preprocessing-createtablescores.R
+++ R/preprocessing-createtablescores.R
@@ -85,13 +85,40 @@
 #' scores. The resulting table also includes annotations for each transcript.
 #'
 #' @examples
-#' # Example usage:
-#' # tmpfold <- "path/to/tmp/folder"
-#' # exptabpath <- "path/to/experiment_table.csv"
-#' # finaltab <- createtablescores(tmpfold = tmpfold, exptabpath = exptabpath,
-#' #   showmemory = TRUE, showtime = TRUE, savefinaltable = TRUE,
-#' #   finaltabpath = "./results", finaltabname = "final_scores.tsv",
-#' #   verbose = TRUE)
+#' \donttest {
+#' exptabpath <- system.file("extdata", "exptab-preprocessing.csv", package="tepr")
+#' gencodepath <- system.file("extdata", "gencode-chr13.gtf", package = "tepr")
+#' maptrackpath <- system.file("extdata", "k50.umap.chr13.hg38.0.8.bed",
+#' package = "tepr")
+#' blacklistpath <- system.file("extdata", "hg38-blacklist-chr13.v2.bed",
+#'     package = "tepr")
+#' tmpfoldpath <- file.path(getwd(), "tmptepr")
+#' windsize <- 200
+#' genomename <- "hg38"
+#' chromtabtest <- rtracklayer::SeqinfoForUCSCGenome(genomename)
+#' allchromvec <- GenomeInfoDb::seqnames(chromtabtest)
+#' chromtabtest <- chromtabtest[allchromvec[which(allchromvec == "chr13")], ]
+#' 
+#' ## Copying bedgraphs to the current directory
+#' expdfpre <- read.csv(exptabpath)
+#' bgpathvec <- sapply(expdfpre$path, function(x) system.file("extdata", x,
+#'     package = "tepr"))
+#' expdfpre$path <- bgpathvec
+#' write.csv(expdfpre, file = "exptab-preprocessing.csv", row.names = FALSE,
+#'     quote = FALSE)
+#' exptabpath <- "exptab-preprocessing.csv"
+#'
+#' ## Necessary result to call createtablescores
+#' allannobed <- retrieveanno(exptabpath, gencodepath, verbose = FALSE)
+#' allwindowsbed <- makewindows(allannobed, windsize, verbose = FALSE)
+#' blacklisthighmap(maptrackpath, blacklistpath, exptabpath, nbcputrans = 1,
+#'     allwindowsbed, windsize, genomename = genomename, chromtab = chromtabtest,
+#'     verbose = FALSE)
+#'
+#' ## Calling the function to test
+#' finaltabtest <- createtablescores(tmpfold = tmpfoldpath, exptabpath,
+#'     savefinaltable = FALSE, verbose = FALSE)
+#' }
 #'
 #' @importFrom dplyr full_join
 #' @importFrom utils read.csv
