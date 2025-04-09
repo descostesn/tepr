887e8504179b857c973e4e8262689966e2001917
descoste <nicolas.descostes@embl.it>
2025-04-09 10:27:07 +0200

    update example

--- R/plotauc.R
+++ R/plotauc.R
@@ -138,8 +138,30 @@
 #'  output folder.
 #'
 #' @examples
-#' # Assuming `tab` contains AUC values and p-values:
-#' # plotauc(tab, expdf, genevec = c("Gene1", "Gene2"), plottype = "pval")
+#' exppath <-  system.file("extdata", "exptab.csv", package="tepr")
+#' transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
+#' expthres <- 0.1
+#'
+#' ## Calculating necessary results
+#' expdf <- read.csv(exppath)
+#' transdf <- read.delim(transpath, header = FALSE)
+#' avfilt <- averageandfilterexprs(expdf, transdf, expthres,
+#'        showtime = FALSE, verbose = FALSE)
+#' rescountna <- countna(avfilt, expdf, nbcpu = 1, verbose = FALSE)
+#' ecdf <- genesECDF(avfilt, expdf, verbose = FALSE)
+#' resecdf <- ecdf[[1]]
+#' nbwindows <- ecdf[[2]]
+#' resmeandiff <- meandifference(resecdf, expdf, nbwindows,
+#'    verbose = FALSE)
+#' bytranslistmean <- split(resmeandiff, factor(resmeandiff$transcript))
+#' resknee <- kneeid(bytranslistmean, expdf, verbose = FALSE)
+#' resauc <- allauc(bytranslistmean, expdf, nbwindows, verbose = FALSE)
+#' resatt <- attenuation(resauc, resknee, rescountna, bytranslistmean, expdf,
+#'        resmeandiff, verbose = FALSE)
+#' resug <- universegroup(resatt, expdf, verbose = FALSE)
+#'
+#' ## Testing plotauc
+#' plotauc(resug, expdf, plottype = "groups", plot = TRUE)
 #'
 #' @seealso
 #' [allauc]
