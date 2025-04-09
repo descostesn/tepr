67da761c1355dbd58c2ac7d29b3b3ef552950ef7
descoste <nicolas.descostes@embl.it>
2025-04-09 11:19:47 +0200

    update example

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -113,19 +113,29 @@
 #' Temporary files created during processing are optionally deleted at the end.
 #'
 #' @examples
-#' # Example usage of preprocessing:
-#' # preprocessing(
-#' #   exptabpath = "example_exptab.tsv",
-#' #   gencodepath = "gencode.v38.annotation.gtf",
-#' #   windsize = 200,
-#' #   maptrackpath = "mappability_track.bed",
-#' #   blacklistpath = "blacklist.bed",
-#' #   genomename = "hg38", nbcputrans = 2, finaltabpath = "results",
-#' #   finaltabname = "final_annotated_table.tsv",
-#' #   tmpfold = file.path(getwd(), "tmptepr"),
-#' #   saveobjectpath = "saved_objects", savefinaltable = TRUE,
-#' #   reload = FALSE, showtime = TRUE, showmemory = TRUE, deletetmp = TRUE,
-#' #   verbose = TRUE)
+#' \donttest {
+#' ## Data
+#' exptabpath <- system.file("extdata", "exptab-preprocessing.csv", package = "tepr")
+#' gencodepath <- system.file("extdata", "gencode-chr13.gtf", package = "tepr")
+#' maptrackpath <- system.file("extdata", "k50.umap.chr13.hg38.0.8.bed",
+#'   package = "tepr")
+#' blacklistpath <- system.file("extdata", "hg38-blacklist-chr13.v2.bed",
+#'     package = "tepr")
+#' windsize <- 200
+#' genomename <- "hg38"
+#'
+#' ## Copying bedgraphs to the current directory
+#' expdfpre <- read.csv(exptabpath)
+#' bgpathvec <- sapply(expdfpre$path, function(x) system.file("extdata", x,
+#'     package = "tepr"))
+#' expdfpre$path <- bgpathvec
+#' write.csv(expdfpre, file = "exptab-preprocessing.csv", row.names = FALSE,
+#'     quote = FALSE)
+#' exptabpath <- "exptab-preprocessing.csv"
+#'
+#' ## Testing preprocessing
+#' finaltabtest <- preprocessing(exptabpath, gencodepath, windsize, maptrackpath,
+#'     blacklistpath, genomename = genomename, verbose = FALSE)
 #'
 #' @seealso
 #' [retrieveanno], [makewindows], [blacklisthighmap], [createtablescores]
