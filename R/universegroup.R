f866d28906c10a02f2ef1ef82e784498306474f9
descoste <nicolas.descostes@embl.it>
2025-04-09 11:33:10 +0200

    update example

--- R/universegroup.R
+++ R/universegroup.R
@@ -75,9 +75,28 @@
 #' on their transcriptional response to different experimental conditions.
 #'
 #' @examples
-#' # Example usage:
-#' # classified_df <- universegroup(completedf, expdf, controlname = "ctrl",
-#' # stressname = "HS")
+#' exppath <-  system.file("extdata", "exptab.csv", package="tepr")
+#' transpath <- system.file("extdata", "cugusi_6.tsv", package="tepr")
+#' expthres <- 0.1
+#'
+#' ## Calculating necessary results
+#' expdf <- read.csv(exppath)
+#' transdf <- read.delim(transpath, header = FALSE)
+#' avfilt <- averageandfilterexprs(expdf, transdf, expthres,
+#'        showtime = FALSE, verbose = FALSE)
+#' rescountna <- countna(avfilt, expdf, nbcpu = 1, verbose = FALSE)
+#' ecdf <- genesECDF(avfilt, expdf, verbose = FALSE)
+#' resecdf <- ecdf[[1]]
+#' nbwindows <- ecdf[[2]]
+#' resmeandiff <- meandifference(resecdf, expdf, nbwindows,
+#'     verbose = FALSE)
+#' bytranslistmean <- split(resmeandiff, factor(resmeandiff$transcript))
+#' resknee <- kneeid(bytranslistmean, expdf, verbose = FALSE)
+#' resauc <- allauc(bytranslistmean, expdf, nbwindows, verbose = FALSE)
+#' resatt <- attenuation(resauc, resknee, rescountna, bytranslistmean, expdf,
+#'         resmeandiff, verbose = FALSE)
+#' ## Testing universegroup
+#' resug <- universegroup(resatt, expdf, verbose = FALSE)
 #'
 #' @seealso
 #' [attenuation]
