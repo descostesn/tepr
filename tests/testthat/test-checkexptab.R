44e1a24a3939b8cdb1f543b49b37ff19a3372d08
descostesn <nicolas.descostes@gmail.com>
2025-10-29 16:09:48 -0400

    second error

--- tests/testthat/test-checkexptab.R
+++ tests/testthat/test-checkexptab.R
@@ -10,5 +10,11 @@ test_that("Errors are thrown when calling checkexptab", {
     expm <- paste0("The experiment table should have the columns: 'condition',",
         " 'replicate', 'direction', 'strand', 'path'.")
     expect_error(checkexptab(exptest), regexp = expm)
+
+    exptest <- expdf
+    exptest[1, 3] <- "toto"
+    expm <- paste0("\n\t Only two values are allowed for the column direction",
+        " of the experiment table, 'forward' and 'reverse'.\n")
+    expect_error(checkexptab(exptest), regexp = expm)
 })
 
