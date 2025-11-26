a5c15889ae5aa2e12e061bf25a4f5422ade0c834
ndescostes <nicolas.descostes@gmail.com>
2025-11-26 13:01:37 -0500

    Add saving functionality for filtered datasets and enhance tepr analysis for depleted conditions

--- tmp/RDScreation_dTag.R
+++ tmp/RDScreation_dTag.R
@@ -70,6 +70,9 @@ expdftwo <- expdf %>% filter(condition=="depletedctrl" | condition=="depletedHS"
 transdftwo <- transdf %>% select(-(V34:V57))
 restwo <- tepr(expdftwo, alldf=transdftwo, expthres, controlcondname = "depletedctrl", stresscondname = "depletedHS")
 
+saveRDS(expdftwo, file="expdftwo.RDS")
+saveRDS(transdftwo, file="transdftwo.RDS")
+
 ###
 # teprmulti on all conditions
 ###
@@ -80,3 +83,30 @@ resmulti <- teprmulti(expdf, alldf=transdf, expthres)
 ## Debug
 
 alldf=transdf; nbcpu = 1; rounding = 10; dontcompare = NULL; replaceval = NA; pval = 0.1; significant = FALSE; windsizethres = 50; countnathres = 20; pvaltheorythres = 0.1; meancond1thres = 0.5; meancond2thres = 0.5; auccond1threshigher = -10; auccond1threslower = 15; auccond2thres = 15; attenuatedpvalksthres = 2; outgrouppvalksthres = 0.2; saveobjectpath = NA; reload = FALSE; showtime = FALSE; showmemory = FALSE; verbose = TRUE
+
+dontcompare <- c("depletedctrl_vs_wtctrl", "depletedctrl_vs_wtHS", "depletedHS_vs_wtctrl", "depletedHS_vs_wtHS")
+
+currentcol <- matcond[ ,1]
+
+
+
+tepr(expdf = expdf2cond, alldf = alldf2cond, expthres = expthres, nbcpu = nbcpu, rounding = rounding,
+            controlcondname = cond1name, stresscondname = cond2name,
+            replaceval = replaceval, pval = pval, significant = significant,
+            windsizethres = windsizethres, countnathres = countnathres,
+            meanctrlthres = meancond1thres, meanstressthres = meancond2thres,
+            pvaltheorythres = pvaltheorythres,
+            aucctrlthreshigher = auccond1threshigher,
+            aucctrlthreslower = auccond1threslower,
+            aucstressthres = auccond2thres,
+            attenuatedpvalksthres = attenuatedpvalksthres,
+            outgrouppvalksthres = outgrouppvalksthres, showtime = showtime,
+            verbose = verbose)
+
+tepr(expdf = expdf2cond, alldf = alldf2cond, expthres = expthres, controlcondname = cond1name, stresscondname = cond2name)
+
+expdftwo <- expdf %>% filter(condition=="depletedctrl" | condition=="depletedHS")
+transdftwo <- transdf %>% select(-(V34:V57))
+
+isTRUE(all.equal(expdf2cond, expdftwo))
+isTRUE(all.equal(alldf2cond, transdftwo))
