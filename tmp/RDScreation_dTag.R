57ef50a76166bb22f73fa7dddc50f0a98338aedd
ndescostes <nicolas.descostes@gmail.com>
2025-11-26 11:16:13 -0500

    tepr and teprmulti

--- tmp/RDScreation_dTag.R
+++ tmp/RDScreation_dTag.R
@@ -2,8 +2,10 @@
 ## FOR WT
 
 ## Read input tables for WT 
-expdf <- read.csv("/Users/S238924/Documents/Other/PhD/PhD_papers/dTAG/expdf_dTag_corrected.csv", header = T, stringsAsFactors = FALSE) %>% filter(condition=="wtctrl" | condition=="wtHS")
-transdf <- read.delim("/Users/S238924/Documents/Other/PhD/PhD_papers/dtag.tsv", header = FALSE) %>% select(-(V10:V33))
+expdf <- read.csv("expdf_dTag_corrected.csv", header = T, stringsAsFactors = FALSE) %>% filter(condition=="wtctrl" | condition=="wtHS")
+expdf$path=gsub("/Users/S238924/Documents/Other/PhD/PhD_papers/dTAG/","",expdf$path)
+
+transdf <- read.delim("dtag.tsv", header = FALSE) %>% select(-(V10:V33))
 
 ## Calculate Average Expression and Filter Transcript Data
 expthres <- 0.1
@@ -23,8 +25,9 @@ saveRDS(resecdflist, file = "/Users/S238924/Documents/Other/PhD/PhD_papers/resec
 ## Read input tables for DEPLETED ##
 ###############################
 
-expdf <- read.csv("/Users/S238924/Documents/Other/PhD/PhD_papers/dTAG/expdf_dTag_corrected.csv", header = T, stringsAsFactors = FALSE) %>% filter(condition=="depletedctrl" | condition=="depletedHS")
-transdf <- read.delim("/Users/S238924/Documents/Other/PhD/PhD_papers/dtag.tsv", header = FALSE) %>% select(-(V34:V57))
+expdf <- read.csv("expdf_dTag_corrected.csv", header = T, stringsAsFactors = FALSE) %>% filter(condition=="depletedctrl" | condition=="depletedHS")
+transdf <- read.delim("dtag.tsv", header = FALSE) %>% select(-(V34:V57))
+expdf$path=gsub("/Users/S238924/Documents/Other/PhD/PhD_papers/dTAG/","",expdf$path)
 
 ## Calculate Average Expression and Filter Transcript Data
 expthres <- 0.1
@@ -40,3 +43,22 @@ resecdf <- resecdflist[[1]]
 
 saveRDS(resecdflist, file = "/Users/S238924/Documents/Other/PhD/PhD_papers/resecdflist_depleted_t0.1.dTAG") #threshold 0.1 for depleted ctrl and depleted hs
 
+
+########################
+## tepr and teprmulti
+
+## Read input tables for WT 
+expdf <- read.csv("expdf_dTag_corrected.csv", header = T, stringsAsFactors = FALSE)
+expdf$path=gsub("/Users/S238924/Documents/Other/PhD/PhD_papers/dTAG/","",expdf$path)
+transdf <- read.delim("dtag.tsv", header = FALSE)
+
+## Calculate Average Expression and Filter Transcript Data
+expthres <- 0.1
+
+## Keep first
+expdfone <- expdf %>% filter(condition=="wtctrl" | condition=="wtHS")
+transdfone <- transdf %>% select(-(V10:V33))
+
+## Keep second
+expdftwo <- expdf %>% filter(condition=="depletedctrl" | condition=="depletedHS")
+transdftwo <- transdf %>% select(-(V34:V57))
