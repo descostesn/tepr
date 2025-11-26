57e5de8151f9cf5a59c32775b31757eb3ebc81ef
ndescostes <nicolas.descostes@gmail.com>
2025-11-26 13:07:11 -0500

    Set up column names for tepr function testing in RDScreation_dTag.R

--- tmp/RDScreation_dTag.R
+++ tmp/RDScreation_dTag.R
@@ -110,3 +110,6 @@ transdftwo <- transdf %>% select(-(V34:V57))
 
 isTRUE(all.equal(expdf2cond, expdftwo))
 isTRUE(all.equal(alldf2cond, transdftwo))
+
+# Set up data for tepr function testing
+colnames(alldf2cond) <- c("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17", "V18", "V19", "V20", "V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28", "V29", "V30", "V31", "V32", "V33")
